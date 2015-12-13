602a5af928bae83f2aeaa57b63eba91e03a4fbb9
Kelsey <kelseyoo14@gmail.com>
2015-12-13 13:59:12 -0800

    added basic sign up page beginnings

---
+++ js/SignUp.js
@@ -0,0 +1,12 @@
+function printingValues()
+{
+	var first = document.getElementById("namefirst").value;
+	var last = document.reg.firstname.value;
+	var email = document.reg.email.value;
+	var password = document.reg.pwd.value;
+
+	console.log(first);
+	console.log(last);
+	console.log(email);
+	console.log(password);
+}

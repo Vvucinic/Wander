0620ad2861608facfeef3f077976969047e2d1d5
Duhkka <vanja.vucinic@roche.com>
2015-12-22 10:00:21 -0800

    Updated virtual environment, slight modifications to settings/urls

--- wander/urls.py
+++ wander/urls.py
@@ -18,6 +18,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+            url(r'^polls/', include('polls.urls')),
             url(r'^adduser/', include('adduser.urls')),
                 url(r'^admin/', admin.site.urls),
                 ]
